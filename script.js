3658d4257c43f0b9eb4d1f3047aacc7d257d3c0b
João Victor Kulka <jv.kulka13@gmail.com>
2023-11-02 19:29:52 -0300

    First Commit
    Iniciado realização de exercícios curso TypeScript Origamid

---
+++ script.js
@@ -0,0 +1,45 @@
+"use strict";
+// Exércicio 1 - Annotation e Inference
+function normalizarTexto(texto) {
+    return texto.trim().toLowerCase();
+}
+normalizarTexto("DEsiGN");
+// Exércicio 2 - Annotation e Inference
+const input = document.querySelector("input");
+const total = localStorage.getItem("total");
+if (input && total) {
+    input.value = total;
+    calcularGanho(Number(input.value));
+}
+function calcularGanho(value) {
+    const p = document.querySelector("p");
+    if (p) {
+        p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+    }
+}
+function totalMudou() {
+    if (input) {
+        localStorage.setItem("total", input.value);
+        calcularGanho(Number(input.value));
+    }
+}
+if (input) {
+    input.addEventListener("keyup", totalMudou);
+}
+// Exércicio 3 - Union Types
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+function toNumber(value) {
+    if (typeof value === "number") {
+        return value;
+    }
+    else if (typeof value === "string") {
+        return Number(value);
+    }
+    else {
+        throw "value deve ser um número ou uma string";
+    }
+}
